4e024ff56edf338653feaa73b775f57c4a07b17d
Desiree Betts <anibetts10@gmail.com>
2019-04-03 22:13:43 +0000

    first

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
+  var search = $("#search-term").val();
+  var myNumber = Math.floor(Math.random() * 5);
+  $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+            $('.container').append("<img src=" +  response.data[myNumber].images.fixed_height.url + ">");
+            },
+         }); 
+    
+    });
+    
 
